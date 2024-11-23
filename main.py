5e0fe9aac212b03f7277570d8c7283972018a568
Sai-Harshith-15 <saiharshith15.git@gmail.com>
2024-11-23 19:05:12 +0530

    updated code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from api import app as api_router  
 import uvicorn
+import os
 app = FastAPI()
 
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000,)
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
